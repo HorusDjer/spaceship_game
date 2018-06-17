e9ac922d3a1b671ad6aec1852fbbdf2f366dc2f4
Ejike <obineme@me.com>
2018-06-17 12:21:17 -0400

    changed game nomenclature

--- spaceship_game.py
+++ spaceship_game.py
@@ -12,7 +12,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
+    pygame.display.set_caption("Spaceship Game")
     
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
