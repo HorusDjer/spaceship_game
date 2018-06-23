69d338116d7a61815fb7b8ff0ce8d8ed30bbdb63
HorusDjer <eobineme@gmail.com>
2018-06-23 08:18:08 -0400

    changed nomenclature

--- space_journey.py
+++ space_journey.py
@@ -10,7 +10,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption('Alien Invasion')
+    pygame.display.set_caption('Space Journey')
 
     # Make a ship, a group of bullets and a group of aliens.
     ship = Ship(ai_settings, screen)
