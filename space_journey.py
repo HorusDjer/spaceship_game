e2ce117dd6102af9375e4d062314ead8236f4bfe
Ejike <obineme@me.com>
2018-06-22 21:37:53 -0400

    created alternate game start

---
+++ space_journey.py
@@ -0,0 +1,32 @@
+import pygame # Use print(pygame.__file__) to file location
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    """Initialize game, settings and create a screen object."""
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption('Alien Invasion')
+
+    # Make a ship, a group of bullets and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+if __name__ == '__main__':
+    run_game()
